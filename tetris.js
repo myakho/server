1911368d379ce96058b4b26d81e5d4c120771ff8
myakho <168271890+myakho@users.noreply.github.com>
2024-06-06 08:53:14 +0900

    Update tetris.js
    Signed-off-by: myakho <168271890+myakho@users.noreply.github.com>

--- tetris.js
+++ tetris.js
@@ -11,12 +11,12 @@ canvas.width = cols * scale;
 canvas.height = rows * scale;
 
 context.scale(scale, scale);
-nextContext.scale(100000, 100000); // 다음 블럭을 더 크게 표시
+nextContext.scale(1000000000000, 10000000000000); // 다음 블럭을 더 크게 표시
 
 const arena = createMatrix(cols, rows);
 
 const player = {
-    pos: { x: 0, y: 0 },
+    pos: { x: 50, y: -50 },
     matrix: null,
     next: null,
     score: 0,
