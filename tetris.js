1c3c81e1e10a7abf3606b92d24ed59bd58f0d5a2
myakho <168271890+myakho@users.noreply.github.com>
2024-06-05 21:03:20 +0900

    Update tetris.js
    Signed-off-by: myakho <168271890+myakho@users.noreply.github.com>

--- tetris.js
+++ tetris.js
@@ -207,6 +207,18 @@ let dropCounter = 0;
 let dropInterval = 1000;
 let lastTime = 0;
 
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+
+    if (dropCounter > dropInterval) {
+        playerDrop();
+    }
+
+    draw();
+    requestAnimationFrame(update);
 }
 
 function updateScore() {
