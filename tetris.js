79886dc3a85bca5a66692c75c1b8f71aabc1789e
myakho <168271890+myakho@users.noreply.github.com>
2024-06-05 14:34:04 +0900

    Add files via upload
    Signed-off-by: myakho <168271890+myakho@users.noreply.github.com>

---
+++ tetris.js
@@ -0,0 +1,404 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+
+const ROW = 20;
+const COL = COLUMN = 10;
+const SQ = squareSize = 30;
+const VACANT = "WHITE"; // empty square
+
+// draw a square
+function drawSquare(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x * SQ, y * SQ, SQ, SQ);
+
+    context.strokeStyle = "BLACK";
+    context.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+
+// create the board
+let board = [];
+for (let r = 0; r < ROW; r++) {
+    board[r] = [];
+    for (let c = 0; c < COL; c++) {
+        board[r][c] = VACANT;
+    }
+}
+
+// draw the board
+function drawBoard() {
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
+            drawSquare(c, r, board[r][c]);
+        }
+    }
+}
+
+drawBoard();
+
+// the pieces and their colors
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"]
+];
+
+// generate random pieces
+function randomPiece() {
+    let r = Math.floor(Math.random() * PIECES.length); // 0 -> 6
+    return new Piece(PIECES[r][0], PIECES[r][1]);
+}
+
+let p = randomPiece();
+
+// The Object Piece
+function Piece(tetromino, color) {
+    this.tetromino = tetromino;
+    this.color = color;
+
+    this.tetrominoN = 0; // we start from the first pattern
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+
+    // we need to control the pieces
+    this.x = 3;
+    this.y = -2;
+}
+
+// fill function
+Piece.prototype.fill = function (color) {
+    for (let r = 0; r < this.activeTetromino.length; r++) {
+        for (let c = 0; c < this.activeTetromino.length; c++) {
+            if (this.activeTetromino[r][c]) {
+                drawSquare(this.x + c, this.y + r, color);
+            }
+        }
+    }
+}
+
+// draw a piece to the board
+Piece.prototype.draw = function () {
+    this.fill(this.color);
+}
+
+// undraw a piece
+Piece.prototype.unDraw = function () {
+    this.fill(VACANT);
+}
+
+// move Down the piece
+Piece.prototype.moveDown = function () {
+    if (!this.collision(0, 1, this.activeTetromino)) {
+        this.unDraw();
+        this.y++;
+        this.draw();
+    } else {
+        this.lock();
+        p = randomPiece();
+    }
+}
+
+// move Right the piece
+Piece.prototype.moveRight = function () {
+    if (!this.collision(1, 0, this.activeTetromino)) {
+        this.unDraw();
+        this.x++;
+        this.draw();
+    }
+}
+
+// move Left the piece
+Piece.prototype.moveLeft = function () {
+    if (!this.collision(-1, 0, this.activeTetromino)) {
+        this.unDraw();
+        this.x--;
+        this.draw();
+    }
+}
+
+// rotate the piece
+Piece.prototype.rotate = function () {
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+    let kick = 0;
+
+    if (this.collision(0, 0, nextPattern)) {
+        if (this.x > COL / 2) {
+            kick = -1; // move the piece to the left
+        } else {
+            kick = 1; // move the piece to the right
+        }
+    }
+
+    if (!this.collision(kick, 0, nextPattern)) {
+        this.unDraw();
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.draw();
+    }
+}
+
+let dropStart = Date.now();
+let gameOver = false;
+function drop() {
+    let now = Date.now();
+    let delta = now - dropStart;
+    if (delta > 1000) {
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    if (!gameOver) {
+        requestAnimationFrame(drop);
+    }
+}
+
+drop();
+
+// collision function
+Piece.prototype.collision = function (x, y, piece) {
+    for (let r = 0; r < piece.length; r++) {
+        for (let c = 0; c < piece.length; c++) {
+            if (!piece[r][c]) {
+                continue;
+            }
+            let newX = this.x + c + x;
+            let newY = this.y + r + y;
+
+            if (newX < 0 || newX >= COL || newY >= ROW) {
+                return true;
+            }
+            if (newY < 0) {
+                continue;
+            }
+            if (board[newY][newX] != VACANT) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event) {
+    if (event.keyCode == 37) {
+        p.moveLeft();
+        dropStart = Date.now();
+    } else if (event.keyCode == 38) {
+        p.rotate();
+        dropStart = Date.now();
+    } else if (event.keyCode == 39) {
+        p.moveRight();
+        dropStart = Date.now();
+    } else if (event.keyCode == 40) {
+        p.moveDown();
+    }
+}
+
+Piece.prototype.lock = function () {
+    for (let r = 0; r < this.activeTetromino.length; r++) {
+        for (let c = 0; c < this.activeTetromino.length; c++) {
+            if (!this.activeTetromino[r][c]) {
+                continue;
+            }
+            if (this.y + r < 0) {
+                alert("Game Over");
+                // stop request animation frame
+                gameOver = true;
+                break;
+            }
+            board[this.y + r][this.x + c] = this.color;
+        }
+    }
+    for (let r = 0; r < ROW; r++) {
+        let isRowFull = true;
+        for (let c = 0; c < COL; c++) {
+            isRowFull = isRowFull && (board[r][c] != VACANT);
+        }
+        if (isRowFull) {
+            for (let y = r; y > 1; y--) {
+                for (let c = 0; c < COL; c++) {
+                    board[y][c] = board[y - 1][c];
+                }
+            }
+            for (let c = 0; c < COL; c++) {
+                board[0][c] = VACANT;
+            }
+        }
+    }
+    drawBoard();
+}
+
+// Tetrominoes
+const I = [
+    [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ]
+];
+
+const J = [
+    [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
+];
+
+const L = [
+    [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const O = [
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+];
+
+const S = [
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+    ],
+    [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const T = [
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const Z = [
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 0, 0]
+    ]
+];
+
+// Mobile controls
+document.getElementById('left').addEventListener('click', () => {
+    p.moveLeft();
+    dropStart = Date.now();
+});
+
+document.getElementById('rotate').addEventListener('click', () => {
+    p.rotate();
+    dropStart = Date.now();
+});
+
+document.getElementById('right').addEventListener('click', () => {
+    p.moveRight();
+    dropStart = Date.now();
+});
+
+document.getElementById('down').addEventListener('click', () => {
+    p.moveDown();
+});
